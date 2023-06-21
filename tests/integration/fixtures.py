868018f191ddff47d9702c6162d3a5ea695ae27d
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2023-06-21 10:16:29 +0200

    Excluding COS

--- tests/integration/fixtures.py
+++ tests/integration/fixtures.py
@@ -2,38 +2,14 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import json
-import os
-
 import pytest
-import requests
-from jinja2 import Environment, FileSystemLoader
 from pytest_operator.plugin import OpsTest
+from webui_helper import WebUI
 
-os.environ["KUBECONFIG"] = "/var/snap/microk8s/current/credentials/client.config"
-
-COS_MODEL_NAME = "cos-lite"
 GNBSIM_MODEL_NAME = "simulator"
 TEST_DEVICE_GROUP_NAME = "integration_tests"
 TEST_IMSI = "208930100007487"
 TEST_NETWORK_SLICE_NAME = "e2e"
-JINJA_ENV = Environment(loader=FileSystemLoader("tests/integration/resources"))
-
-
-@pytest.fixture(scope="module")
-@pytest.mark.abort_on_fail
-async def deploy_cos(ops_test: OpsTest):
-    """Deploys observability model.
-
-    COS model includes:
-    - cos-lite bundle
-    - cos-configuration-k8s charm providing custom Charmed SD-Core dashboard
-
-    Args:
-        ops_test: OpsTest
-    """
-    await deploy_cos_lite(ops_test)
-    await deploy_cos_configuration(ops_test)
 
 
 @pytest.fixture(scope="module")
@@ -50,21 +26,20 @@ async def configure_sdcore(ops_test: OpsTest):
         ops_test: OpsTest
     """
     webui_ip_address = await get_unit_address(ops_test, "webui", 0)
-    create_subscriber_resp = _create_subscriber(webui_ip_address)
-    assert create_subscriber_resp.status_code == 201
-    create_device_group_resp = _create_device_group(webui_ip_address)
-    assert create_device_group_resp.status_code == 200
-    create_network_slice_resp = _create_network_slice(webui_ip_address)
-    assert create_network_slice_resp.status_code == 200
+    webui_client = WebUI(webui_ip_address)
+    webui_client.create_subscriber(TEST_IMSI)
+    webui_client.create_device_group(TEST_DEVICE_GROUP_NAME, [TEST_IMSI])
+    webui_client.create_network_slice(TEST_NETWORK_SLICE_NAME, [TEST_DEVICE_GROUP_NAME])
 
 
 @pytest.fixture(scope="module")
 @pytest.mark.abort_on_fail
-async def deploy_gnbsim(ops_test: OpsTest):
+async def deploy_gnbsim(ops_test: OpsTest, configure_sdcore):
     """Deploys `sdcore-gnbsim`.
 
     Args:
         ops_test: OpsTest
+        configure_sdcore: `configure_sdcore` fixture
     """
     await ops_test.model.deploy(  # type: ignore[union-attr]
         "sdcore-gnbsim",
@@ -77,108 +52,9 @@ async def deploy_gnbsim(ops_test: OpsTest):
         apps=["gnbsim"],
         raise_on_error=False,
         status="active",
+        idle_period=10,
         timeout=300,
     )
-
-
-async def deploy_cos_lite(ops_test: OpsTest):
-    """Deploys `cos-lite` bundle.
-
-    Args:
-        ops_test: OpsTest
-    """
-    await ops_test.track_model(
-        alias=COS_MODEL_NAME,
-        model_name=COS_MODEL_NAME,
-        cloud_name=ops_test.cloud_name,
-    )
-
-    with ops_test.model_context(COS_MODEL_NAME):
-        await ops_test.model.deploy(  # type: ignore[union-attr]
-            entity_url="https://charmhub.io/cos-lite",
-            trust=True,
-        )
-        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
-            apps=[*ops_test.model.applications],  # type: ignore[union-attr]
-            raise_on_error=False,
-            status="active",
-            timeout=600,
-        )
-        await ops_test.model.create_offer(  # type: ignore[union-attr]
-            endpoint="prometheus:receive-remote-write",
-            offer_name="prometheus",
-            application_name="prometheus",
-        )
-
-
-async def deploy_cos_configuration(ops_test: OpsTest):
-    """Deploys `cos-configuration-k8s` charm.
-
-    Args:
-        ops_test: OpsTest
-    """
-    with ops_test.model_context(COS_MODEL_NAME):
-        await ops_test.model.deploy(  # type: ignore[union-attr]
-            "cos-configuration-k8s",
-            config={
-                "git_repo": "https://github.com/canonical/sdcore-cos-configuration",
-                "git_branch": "main",
-                "git_depth": 1,
-                "grafana_dashboards_path": "grafana_dashboards/sdcore/",
-            },
-        )
-        await ops_test.model.add_relation("cos-configuration-k8s", "grafana")  # type: ignore[union-attr]  # noqa: E501
-        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
-            apps=["cos-configuration-k8s"],
-            raise_on_error=False,
-            status="active",
-            timeout=300,
-        )
-
-
-def _create_subscriber(webui_ip: str) -> requests.Response:
-    """Creates a subscriber from a template.
-
-    Args:
-        webui_ip: IP address of the `webui` application unit
-
-    Returns:
-        requests.Response: requests.Response Object
-    """
-    template = JINJA_ENV.get_template("subscriber.json.j2")
-    subscriber_data = template.render(imsi=TEST_IMSI)
-    url = f"http://{webui_ip}:5000/api/subscriber/imsi-{TEST_IMSI}"
-    return requests.post(url=url, data=subscriber_data)
-
-
-def _create_device_group(webui_ip: str) -> requests.Response:
-    """Creates a device group from a template.
-
-    Args:
-        webui_ip: IP address of the `webui` application unit
-
-    Returns:
-        requests.Response: requests.Response Object
-    """
-    template = JINJA_ENV.get_template("device-group.json.j2")
-    device_group_json = json.loads(template.render(imsi=TEST_IMSI))
-    url = f"http://{webui_ip}:5000/config/v1/device-group/{TEST_DEVICE_GROUP_NAME}"
-    return requests.post(url, json=device_group_json)
-
-
-def _create_network_slice(webui_ip: str) -> requests.Response:
-    """Creates a network slice from a template.
-
-    Args:
-        webui_ip: IP address of the `webui` application unit
-
-    Returns:
-        requests.Response: requests.Response Object
-    """
-    template = JINJA_ENV.get_template("network-slice.json.j2")
-    network_slice_json = json.loads(template.render(device_group_name=TEST_DEVICE_GROUP_NAME))
-    url = f"http://{webui_ip}:5000/config/v1/network-slice/{TEST_NETWORK_SLICE_NAME}"
-    return requests.post(url, json=network_slice_json)
 
 
 async def get_unit_address(ops_test: OpsTest, app_name: str, unit_num: int) -> str:
---
+++ tests/integration/webui_helper.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import json
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+SUBSCRIBER_JSON = {
+    "UeId": "PLACEHOLDER",
+    "plmnId": "20801",
+    "opc": "981d464c7c52eb6e5036234984ad0bcf",
+    "key": "5122250214c33e723a5dd523fc145fc0",
+    "sequenceNumber": "16f3b3f70fc2"
+}
+DEVICE_GROUP_JSON = {
+    "imsis": [],
+    "site-info": "demo",
+    "ip-domain-name": "pool1",
+    "ip-domain-expanded": {
+        "dnn": "internet",
+        "ue-ip-pool": "172.250.1.0/16",
+        "dns-primary": "8.8.8.8",
+        "mtu": 1460,
+        "ue-dnn-qos": {
+            "dnn-mbr-uplink": 200000000,
+            "dnn-mbr-downlink": 200000000,
+            "traffic-class": {
+                "name": "platinum",
+                "arp": 6,
+                "pdb": 300,
+                "pelr": 6,
+                "qci": 8
+            }
+        }
+    }
+}
+NETWORK_SLICE_JSON = {
+    "slice-id": {
+        "sst": "1",
+        "sd": "010203"
+    },
+    "site-device-group": [],
+    "site-info": {
+        "site-name": "demo",
+        "plmn": {
+            "mcc": "208",
+            "mnc": "93"
+        },
+        "gNodeBs": [
+            {
+                "name": "demo-gnb1",
+                "tac": 1
+            }
+        ],
+        "upf": {
+            "upf-name": "upf",
+            "upf-port": "8805"
+        }
+    }
+}
+
+
+class WebUI:
+    def __init__(self, webui_ip: str) -> None:
+        """Constructor.
+
+        Args:
+            webui_ip (str): IP address of the WebUI application unit
+        """
+        self.webui_ip = webui_ip
+
+    def create_subscriber(self, imsi: str) -> None:
+        """Creates a subscriber.
+
+        Args:
+            imsi (str): Subscriber's IMSI
+        """
+        SUBSCRIBER_JSON["UeId"] = imsi
+        url = f"http://{self.webui_ip}:5000/api/subscriber/imsi-{imsi}"
+        response = requests.post(url=url, data=json.dumps(SUBSCRIBER_JSON))
+        response.raise_for_status()
+
+    def create_device_group(self, device_group_name: str, imsis: list) -> None:
+        """Creates a device group.
+
+        Args:
+            device_group_name (str): Device group name
+            imsis (list): List of IMSIS to be included in the device group
+        """
+        DEVICE_GROUP_JSON["imsis"] = imsis
+        url = f"http://{self.webui_ip}:5000/config/v1/device-group/{device_group_name}"
+        response = requests.post(url, json=DEVICE_GROUP_JSON)
+        response.raise_for_status()
+
+    def create_network_slice(self, network_slice_name: str, device_groups: list) -> None:
+        """Creates a network slice.
+
+        Args:
+            network_slice_name (str): Network slice name
+            device_groups (list): List of device groups to be included in the network slice
+        """
+        NETWORK_SLICE_JSON["site-device-group"] = device_groups
+        url = f"http://{self.webui_ip}:5000/config/v1/network-slice/{network_slice_name}"
+        response = requests.post(url, json=NETWORK_SLICE_JSON)
+        response.raise_for_status()
