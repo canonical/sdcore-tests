f41b4fe6eda2db48f8b8f052f86856d3e32f1ebc
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2023-08-09 11:19:23 +0200

    Testing with custom upf-bess image

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -44,8 +44,9 @@ class TestSDCoreBundle:
         """
         await self._deploy_sdcore_router(ops_test)
         await ops_test.model.deploy(  # type: ignore[union-attr]
-            entity_url="https://charmhub.io/sdcore",
-            channel="latest/edge",
+            entity_url="./tests/integration/bundle.yaml",
+            # entity_url="https://charmhub.io/sdcore",
+            # channel="latest/edge",
             trust=True,
         )
 
