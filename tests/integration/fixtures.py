b3c6fe890b7c5c5ce619892c12da97c8a6d6fa13
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2023-08-22 15:42:21 +0200

    Implements first SD-Core E2E tests (#18)
    * Initial commit
    
    * Run CI on every push
    
    * Adding setup
    
    * Adding gnbsim
    
    * Adding grafana config
    
    * Cleaning up
    
    * Changing runner to self-hosted
    
    * Updating runner config
    
    * Refactoring
    
    * Updating runner's label
    
    * Excluding COS
    
    * Fixing lint
    
    * Adding debug msg
    
    * Adding debug msg
    
    * Adding debug ssh
    
    * Fixing SD-Core configuration
    
    * Removing ssh session
    
    * Adding 1 sec sleep after creation of device group and network slice
    
    * Fixing name of the test
    
    * Getting rid of sleeps
    
    * Unifying waits
    
    * Increasing idle time for gnbsim
    
    * Archive crashdump every time
    
    * Adding upterm sessiona again
    
    * Testing with custom GH runners
    
    * Disabling upterm session
    
    * Fixing typo
    
    * Bumping version of configure-aws-credentials action
    
    * Replacing aws-region secret with hardcode
    
    * Testing with hardcoded role
    
    * Adding double-quotes to secrets refs
    
    * Adding secrets inheritance
    
    * Changing AZ
    
    * Changing AZ
    
    * Changing envs to secrets
    
    * Replacing setup-lxd action with custom impl
    
    * Adding custom operattor env setup
    
    * Keep runner after failure
    
    * ADding ssh key for debugging
    
    * Adding custom impl of ec2-github-runner action to support disk size changes
    
    * Fixing tox command
    
    * Enabling VM teardown
    
    * Increasing timout for SD-Core deployment
    
    * Enabling full CI back
    
    * Lock VM for debugging
    
    * Dont kill the machine
    
    * Kill VM again
    
    * Reverting to default runners as the problem is in different place
    
    * Enabling CI on push
    
    * Adding upterm session for debugging; removing ec2-github-runner action
    
    * Commenting out the upterm session
    
    * Testing with custom upf-bess image
    
    * Adding upterm back
    
    * Removing custom bundle and upterm
    
    * Using custom runners again
    
    * Testing bess rock built for haswell
    
    * Testing with GH runners and bess rock built for haswell
    
    * Moving back to ec2; adding COS
    
    * Adding cross-model relations
    
    * Give COS more time to deploy
    
    * Replacing EC2 runners with Canonical's self-hosted runners
    
    * Disabling CI on push, because self-hosted runners do not support it yet
    
    * Enabling metallb for traefik
    
    * Temporarily removing cos-configuration-k8s from the picture
    
    * Adding more no_proxy hosts
    
    * Reverting to EC2 runners
    
    * Reverting to EC2 runners
    
    * Checking whether actions-operator can be used with a custom runner
    
    * Reverting actions-operator usage

---
+++ tests/integration/fixtures.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import pytest
+from pytest_operator.plugin import OpsTest
+from webui_helper import WebUI
+
+COS_MODEL_NAME = "cos-lite"
+GNBSIM_MODEL_NAME = "simulator"
+TEST_DEVICE_GROUP_NAME = "integration_tests"
+TEST_IMSI = "208930100007487"
+TEST_NETWORK_SLICE_NAME = "e2e"
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.abort_on_fail
+async def deploy_cos(ops_test: OpsTest):
+    """Deploys observability model.
+
+    COS model includes:
+    - cos-lite bundle
+    - cos-configuration-k8s charm providing custom Charmed SD-Core dashboard
+
+    Args:
+        ops_test: OpsTest
+    """
+    await deploy_cos_lite(ops_test)
+    await deploy_cos_configuration(ops_test)
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.abort_on_fail
+async def configure_sdcore(ops_test: OpsTest):
+    """Configures Charmed SD-Core.
+
+    Configuration includes:
+    - subscriber creation
+    - device group creation
+    - network slice creation
+
+    Args:
+        ops_test: OpsTest
+    """
+    webui_ip_address = await get_unit_address(ops_test, "webui", 0)
+    webui_client = WebUI(webui_ip_address)
+    webui_client.create_subscriber(TEST_IMSI)
+    webui_client.create_device_group(TEST_DEVICE_GROUP_NAME, [TEST_IMSI])
+    webui_client.create_network_slice(TEST_NETWORK_SLICE_NAME, [TEST_DEVICE_GROUP_NAME])
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.abort_on_fail
+async def deploy_gnbsim(ops_test: OpsTest, configure_sdcore):
+    """Deploys `sdcore-gnbsim`.
+
+    Args:
+        ops_test: OpsTest
+        configure_sdcore: `configure_sdcore` fixture
+    """
+    await ops_test.model.deploy(  # type: ignore[union-attr]
+        "sdcore-gnbsim",
+        application_name="gnbsim",
+        channel="latest/edge",
+        trust=True,
+    )
+    await ops_test.model.add_relation("gnbsim:fiveg-n2", "amf:fiveg-n2")  # type: ignore[union-attr]  # noqa: E501
+    await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
+        apps=["gnbsim"],
+        raise_on_error=False,
+        status="active",
+        idle_period=10,
+        timeout=300,
+    )
+
+
+async def deploy_cos_lite(ops_test: OpsTest):
+    """Deploys `cos-lite` bundle.
+
+    Args:
+        ops_test: OpsTest
+    """
+    await ops_test.track_model(
+        alias=COS_MODEL_NAME,
+        model_name=COS_MODEL_NAME,
+        cloud_name=ops_test.cloud_name,
+    )
+
+    with ops_test.model_context(COS_MODEL_NAME):
+        await ops_test.model.deploy(  # type: ignore[union-attr]
+            entity_url="https://charmhub.io/cos-lite",
+            trust=True,
+        )
+        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
+            apps=[*ops_test.model.applications],  # type: ignore[union-attr]
+            raise_on_error=False,
+            status="active",
+            timeout=1000,
+        )
+
+        await _create_cross_model_relation_offer(
+            ops_test,
+            model_name=COS_MODEL_NAME,
+            application_name="loki",
+            relation_name="logging",
+        )
+        await _create_cross_model_relation_offer(
+            ops_test,
+            model_name=COS_MODEL_NAME,
+            application_name="prometheus",
+            relation_name="receive-remote-write",
+        )
+
+
+async def deploy_cos_configuration(ops_test: OpsTest):
+    """Deploys `cos-configuration-k8s` charm.
+
+    Args:
+        ops_test: OpsTest
+    """
+    with ops_test.model_context(COS_MODEL_NAME):
+        await ops_test.model.deploy(  # type: ignore[union-attr]
+            "cos-configuration-k8s",
+            config={
+                "git_repo": "https://github.com/canonical/sdcore-cos-configuration",
+                "git_branch": "main",
+                "git_depth": 1,
+                "grafana_dashboards_path": "grafana_dashboards/sdcore/",
+            },
+        )
+        await ops_test.model.add_relation("cos-configuration-k8s", "grafana")  # type: ignore[union-attr]  # noqa: E501
+        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
+            apps=["cos-configuration-k8s"],
+            raise_on_error=False,
+            status="active",
+            timeout=300,
+        )
+
+
+async def get_unit_address(ops_test: OpsTest, app_name: str, unit_num: int) -> str:
+    """Get unit's IP address for any application.
+
+    Args:
+        ops_test: OpsTest
+        app_name: string name of application
+        unit_num: integer number of a juju unit
+
+    Returns:
+        str: Unit's IP address
+    """
+    status = await ops_test.model.get_status()  # type: ignore[union-attr]
+    return status["applications"][app_name]["units"][f"{app_name}/{unit_num}"]["address"]
+
+
+async def _create_cross_model_relation_offer(
+    ops_test: OpsTest, model_name: str, application_name: str, relation_name: str
+) -> None:
+    """Creates a cross-model relation offer.
+
+    Args:
+        ops_test: OpsTest
+        model_name: Provider model name
+        application_name: Provider application
+        relation_name: Provider relation name
+    """
+    offer_run_args = ["juju", "offer", f"{model_name}.{application_name}:{relation_name}"]
+    retcode, stdout, stderr = await ops_test.run(*offer_run_args)
+    if retcode != 0:
+        raise RuntimeError(f"Error: {stderr}")
--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -2,6 +2,117 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import logging
 
-def test_given_when_then():
-    pass
+import pytest
+from fixtures import configure_sdcore, deploy_cos, deploy_gnbsim
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+
+class TestSDCoreBundle:
+    @pytest.mark.abort_on_fail
+    async def test_given_sdcore_bundle_when_deploy_then_status_is_active(
+        self, ops_test: OpsTest, deploy_cos
+    ):
+        await self._deploy_sdcore(ops_test)
+        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
+            apps=[*ops_test.model.applications],  # type: ignore[union-attr]
+            raise_on_error=False,
+            status="active",
+            idle_period=10,
+            timeout=1500,
+        )
+
+    @pytest.mark.abort_on_fail
+    async def test_given_sdcore_bundle_and_gnbsim_deployed_when_start_simulation_then_simulation_success_status_is_true(  # noqa: E501
+        self, ops_test: OpsTest, deploy_gnbsim
+    ):
+        gnbsim_unit = ops_test.model.units["gnbsim/0"]  # type: ignore[union-attr]
+        start_simulation = await gnbsim_unit.run_action("start-simulation")
+        action_output = await ops_test.model.get_action_output(  # type: ignore[union-attr]
+            action_uuid=start_simulation.entity_id, wait=300
+        )
+        assert action_output["success"] == "true"
+
+    async def _deploy_sdcore(self, ops_test: OpsTest):
+        """Deploys `sdcore` bundle.
+
+        Args:
+            ops_test: OpsTest
+        """
+        await self._deploy_sdcore_router(ops_test)
+        await ops_test.model.deploy(  # type: ignore[union-attr]
+            entity_url="https://charmhub.io/sdcore",
+            channel="latest/edge",
+            trust=True,
+        )
+
+        await self._create_cross_model_relation(
+            ops_test,
+            provider_model="cos-lite",
+            offer_name="prometheus",
+            provider_relation_name="receive-remote-write",
+            requirer_app="grafana-agent-k8s",
+            requirer_relation_name="send-remote-write",
+        )
+        await self._create_cross_model_relation(
+            ops_test,
+            provider_model="cos-lite",
+            offer_name="loki",
+            provider_relation_name="logging",
+            requirer_app="grafana-agent-k8s",
+            requirer_relation_name="logging-consumer",
+        )
+
+    @staticmethod
+    async def _deploy_sdcore_router(ops_test: OpsTest):
+        """Deploys `sdcore-router`.
+
+        Args:
+            ops_test: OpsTest
+        """
+        await ops_test.model.deploy(  # type: ignore[union-attr]
+            "sdcore-router",
+            application_name="router",
+            channel="latest/edge",
+            trust=True,
+        )
+        await ops_test.model.wait_for_idle(  # type: ignore[union-attr]
+            apps=["router"],
+            raise_on_error=False,
+            status="active",
+            idle_period=10,
+            timeout=300,
+        )
+
+    @staticmethod
+    async def _create_cross_model_relation(
+        ops_test: OpsTest,
+        provider_model: str,
+        offer_name: str,
+        provider_relation_name: str,
+        requirer_app: str,
+        requirer_relation_name: str,
+    ) -> None:
+        """Creates a cross-model relation.
+
+        1. Consumes an offer created by the relation provider model
+        2. Creates a relation between the provider and the consumer.
+
+        Args:
+            ops_test: OpsTest
+            provider_model: Provider model name
+            offer_name: Relation offer name
+            provider_relation_name: Provider relation name
+            requirer_app: Requirer application name
+            requirer_relation_name: Requirer relation name
+        """
+        consume_run_args = ["juju", "consume", f"{provider_model}.{offer_name}"]
+        retcode, stdout, stderr = await ops_test.run(*consume_run_args)
+        if retcode != 0:
+            raise RuntimeError(f"Error: {stderr}")
+        await ops_test.model.add_relation(  # type: ignore[union-attr]
+            f"{offer_name}:{provider_relation_name}", f"{requirer_app}:{requirer_relation_name}"
+        )
