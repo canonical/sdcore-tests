909b14024905f16defd0f98318de091c918237e3
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-10-16 12:37:37 +0200

    Fixes integration tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,24 +20,33 @@ module "sdcore" {
   depends_on = [module.sdcore-router]
 }
 
+resource "juju_model" "ran-simulator" {
+  name = "ran"
+}
+
 module "gnbsim" {
   source = "git::https://github.com/canonical/sdcore-gnbsim-k8s-operator//terraform"
 
-  model      = juju_model.sdcore.name
+  model      = juju_model.ran-simulator.name
   depends_on = [module.sdcore-router]
 }
 
+resource "juju_offer" "gnbsim-fiveg-gnb-identity" {
+  model            = juju_model.ran-simulator.name
+  application_name = module.gnbsim.app_name
+  endpoint         = module.gnbsim.provides.fiveg_gnb_identity
+}
+
 resource "juju_integration" "gnbsim-amf" {
-  model = juju_model.sdcore.name
+  model = juju_model.ran-simulator.name
 
   application {
     name     = module.gnbsim.app_name
-    endpoint = module.gnbsim.fiveg_n2_endpoint
+    endpoint = module.gnbsim.requires.fiveg_n2
   }
 
   application {
-    name     = module.sdcore.amf_app_name
-    endpoint = module.sdcore.fiveg_n2_endpoint
+    offer_url = module.sdcore.amf_fiveg_n2_offer_url
   }
 }
 
@@ -45,13 +54,12 @@ resource "juju_integration" "gnbsim-nms" {
   model = juju_model.sdcore.name
 
   application {
-    name     = module.gnbsim.app_name
-    endpoint = module.gnbsim.fiveg_gnb_identity_endpoint
+    name     = module.sdcore.nms_app_name
+    endpoint = module.sdcore.fiveg_gnb_identity_endpoint
   }
 
   application {
-    name     = module.sdcore.nms_app_name
-    endpoint = module.sdcore.fiveg_gnb_identity_endpoint
+    offer_url = juju_offer.gnbsim-fiveg-gnb-identity.url
   }
 }
 
